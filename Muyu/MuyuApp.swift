84c945fe7553ed55bf53527b7673524a83268203
Charlie Yang <ilickcat@outlook.com>
2024-11-29 16:28:03 +0800

    Initial Commit

---
+++ Muyu/MuyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuyuApp.swift
+//  Muyu
+//
+//  Created by Chao Yang on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct MuyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
