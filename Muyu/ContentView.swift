2c2b90ef8aeee1573d9dcf053f16a8f4997008f9
Charlie Yang <ilickcat@outlook.com>
2024-12-01 11:11:59 +0800

    define the variable

--- Muyu/ContentView.swift
+++ Muyu/ContentView.swift
@@ -8,7 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.red]
+    let colors: [Color] = [.red, .cyan, .teal, .yellow, .orange, .pink, .green]
+    @State var selectedColor = Color.yellow
+    @State private var _touchCount: Double = 0.0{
+        didSet{
+            if _touchCount == 30.0 {
+                _touchCount = 0.0
+            }
+        }
+    }
+    @State var tapped: Int = 0
+    var touchCount: Double {
+        _touchCount
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -24,5 +37,5 @@ struct ContentView: View {
     ContentView()
 }
 
-//Apple
 //A swift App
+//Powered by Wang YuXiao. Code By Charlie Yang
